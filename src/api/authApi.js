19196611b8141a65c3b6b647092c0f31841ce315
Nguyễn Duy Hào <121230897+duyhaodev@users.noreply.github.com>
2025-10-07 20:35:39 +0700

    cua hao

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -2,7 +2,7 @@ import axiosClient from "./axiosClient";
 
 const authApi = {
     login (data) {
-        const url = "auth/login"
+        const url = "auth/token"
         return axiosClient.post(url,data)
     }
 }
