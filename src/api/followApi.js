1e24f0fc58069b934f5ab286900bb0029b6b75b2
NT-THTRM\nthuy <nthuytram2815@gmail.com>
2025-12-04 19:47:31 +0700

    Redirect trang khi search, hiển thị media, follow

---
+++ src/api/followApi.js
@@ -0,0 +1,16 @@
+// src/api/followApi.js
+import axios from "./axiosClient";
+
+const followApi = {
+  // Lấy trạng thái follow của người dùng hiện tại đối với followingId
+  checkFollowing: (followingId) => {
+    return axios.get(`/api/follow/status/${followingId}`);
+  },
+
+  // Toggle follow/unfollow
+  toggleFollow: (followingId) => {
+    return axios.post(`/api/follow/${followingId}/toggle`);
+  },
+};
+
+export default followApi;
